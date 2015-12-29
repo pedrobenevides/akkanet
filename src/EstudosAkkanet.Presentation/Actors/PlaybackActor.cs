f33a6a393e0b8657336b00dc4ff42a450271e540
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-12-29 17:18:39 -0300

    Criando Actors

---
+++ src/EstudosAkkanet.Presentation/Actors/PlaybackActor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Akka.Actor;
+
+namespace EstudosAkkanet.Presentation.Actors
+{
+    public class PlaybackActor : UntypedActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a Playback Actor");
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if(message is string)
+                Console.WriteLine("Received movie title: " + message);
+            else if (message is int)
+                Console.WriteLine("Received user Id: " + message);
+            else
+                Unhandled(message);
+        }
+    }
+}
--- src/EstudosAkkanet.Presentation/Program.cs
+++ src/EstudosAkkanet.Presentation/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace EstudosAkkanet.Presentation
+﻿using System;
+using Akka.Actor;
+using EstudosAkkanet.Presentation.Actors;
+
+namespace EstudosAkkanet.Presentation
 {
     class Program
     {
+        private static ActorSystem _movieStreamingActorSystem;
+
         static void Main(string[] args)
         {
+            PlaygroundAkka();
+        }
+
+        private static void PlaygroundAkka()
+        {
+            _movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor System created!");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+            IActorRef playbackActorRef = _movieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+            playbackActorRef.Tell("Akka.NET: The Movie");
+            playbackActorRef.Tell(42);
+            playbackActorRef.Tell('c');
+
+            Console.ReadKey();
+            _movieStreamingActorSystem.Shutdown();
         }
     }
 }
